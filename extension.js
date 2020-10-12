cf59d8acb98fe880239d2662d3bc7ad6916e9290
Max <sonthaya.nt@hotmail.com>
2020-10-13 01:15:07 +0700

    Update extension.js

--- extension.js
+++ extension.js
@@ -10,6 +10,15 @@
         "gps_setup",
         "gps_is_ready",
         "gps_position",
-        "gps_speed"
+        "gps_speed",
+        {
+            xml: '<label text="UTC datetime"></label>',
+        },
+        "gps_get_hour",
+        "gps_get_min",
+        "gps_get_sec",
+        "gps_get_day",
+        "gps_get_month",
+        "gps_get_year"
     ]
 });
